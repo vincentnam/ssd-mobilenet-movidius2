adb8837c81bc7da1db1ff5f310fe108918ba326c
vincent-nam <vincent-nam.dang@univ-tlse3.fr>
2019-03-20 10:44:22 +0100

    first commit : get caffemodel

---
+++ test_openvino_SSDMobilenet.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+import time
+import cv2
+
+#caffe_root = '/home/ubuntu/caffe/'
+#image = cv2.imread('/home/ubuntu/caffe/examples/images/cat.jpg')
+#labels_file = caffe_root + 'data/ilsvrc12/synset_words.txt'
+
+model = 'mobilnet_iter_73000.caffemodel'
+prototxt = 'patched_deploi.prototxt'
+
+
